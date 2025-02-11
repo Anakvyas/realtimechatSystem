13e1ba0b7edcd75ba986e54da17cdb945a558e12
Anak Vyas <144206710+Anakvyas@users.noreply.github.com>
2025-02-11 22:27:16 +0530

    Update vite.config.js

--- FRONTEND/vite-project/vite.config.js
+++ FRONTEND/vite-project/vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  
+  server: 
+    historyApiFallback: true,
     proxy: {
       '/api': {
         target: 'https://realtimechatsystem.onrender.com',
