8544b27138d266cc4d9efa316acffac41c0e4c76
Anak Vyas <144206710+Anakvyas@users.noreply.github.com>
2025-02-11 22:54:51 +0530

    Update socket.jsx

--- FRONTEND/vite-project/src/pages/socket.jsx
+++ FRONTEND/vite-project/src/pages/socket.jsx
@@ -1,7 +1,10 @@
 
 import {io} from 'socket.io-client';
 
-const socket = io('https://realtimechatsystem.onrender.com');
+const socket = io('https://realtimechatsystem.onrender.com', {
+    transports: ["websocket"], // Avoids polling issues
+    withCredentials: true // Ensures proper CORS handling
+});
 
 
 export default socket;
